ea533bd232cb40178c9042e9773103cc2f5a7588
emilyvaseva <114073206+emilyvaseva@users.noreply.github.com>
2023-02-22 18:03:12 +0200

    add task

---
+++ src/P01WorkingWithAbstraction/P01RhombusOfStars/rhombusOfStar.java
@@ -0,0 +1,29 @@
+package P01WorkingWithAbstraction.P01RhombusOfStars;
+
+import java.util.Scanner;
+
+public class rhombusOfStar {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        int n = Integer.parseInt(scanner.nextLine());
+
+        for (int i = 1; i <= n; i++) {
+            printRow(i,n-i);
+        }
+
+        for (int i = n-1; i >0 ; i--) {
+            printRow(i,n-i);
+        }
+    }
+
+    private static void printRow(int numberOfStars, int numberOfSpaces) {
+        for (int i = 0; i < numberOfSpaces; i++) {
+            System.out.print(" ");
+        }
+        for (int i = 0; i < numberOfStars; i++) {
+            System.out.print("* ");
+        }
+        System.out.println();
+    }
+}
