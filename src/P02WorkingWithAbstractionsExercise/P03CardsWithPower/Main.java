152bb81e5c92c6c441d638f53c5e18dab451bf33
emilyvaseva <114073206+emilyvaseva@users.noreply.github.com>
2023-02-26 18:47:09 +0200

    add task

---
+++ src/P02WorkingWithAbstractionsExercise/P03CardsWithPower/Main.java
@@ -0,0 +1,19 @@
+package P02WorkingWithAbstractionsExercise.P03CardsWithPower;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String rankString = scanner.nextLine();
+        String suitString = scanner.nextLine();
+
+        Card.Rank rank = Card.Rank.valueOf(rankString);
+        Card.Suit suit = Card.Suit.valueOf(suitString);
+        Card card = new Card(rank,suit);
+
+        System.out.println("Card name: " + card.getName() + "; Card power: " + card.getPower());
+
+    }
+}
