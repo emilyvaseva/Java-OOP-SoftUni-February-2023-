b2c976cdd6a1fcf05acfd3f12326cffe565584fd
emilyvaseva <114073206+emilyvaseva@users.noreply.github.com>
2023-03-07 17:46:45 +0200

    add

---
+++ src/P02Encapsulation/P04FirstAndReserveTeam/Team.java
@@ -0,0 +1,44 @@
+package P02Encapsulation.P04FirstAndReserveTeam;
+
+import P02Encapsulation.P01SortByNameAndAge.Person;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class Team {
+    private String name;
+    private List<Person> firstTeam;
+    private List<Person> reserveTeam;
+
+    Team(String name) {
+        this.firstTeam = new ArrayList<>();
+        this.reserveTeam = new ArrayList<>();
+
+        setName(name);
+    }
+
+    private void setName(String name) {
+        if (name.length() < 5) {
+            throw new IllegalArgumentException("Team name must be at least 5 symbols");
+        }
+
+        this.name = name;
+    }
+
+    void addPlayer(Person player) {
+        if (player.getAge() < 40) {
+            this.firstTeam.add(player);
+        } else {
+            this.reserveTeam.add(player);
+        }
+    }
+
+    List<Person> getFirstTeam() {
+        return Collections.unmodifiableList(firstTeam);
+    }
+
+    public List<Person> getReserveTeam() {
+        return Collections.unmodifiableList(reserveTeam);
+    }
+}
