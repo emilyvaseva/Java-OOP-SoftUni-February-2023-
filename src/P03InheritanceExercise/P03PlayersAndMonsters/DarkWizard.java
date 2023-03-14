8a127f0a8287efd4aed94b7760dc59a1f5e54cc2
emilyvaseva <114073206+emilyvaseva@users.noreply.github.com>
2023-03-14 13:56:14 +0200

    add task

---
+++ src/P03InheritanceExercise/P03PlayersAndMonsters/DarkWizard.java
@@ -0,0 +1,7 @@
+package P03InheritanceExercise.P03PlayersAndMonsters;
+
+public class DarkWizard extends Wizard{
+    public DarkWizard(String username, int level) {
+        super(username, level);
+    }
+}
