ad26d631dbc72f002acedba0ca560d0ebbd69847
emilyvaseva <114073206+emilyvaseva@users.noreply.github.com>
2023-03-14 12:23:45 +0200

    add task

---
+++ src/P04Inheritance/P03RandomArrayList/RandomArrayList.java
@@ -0,0 +1,13 @@
+package P04Inheritance.P03RandomArrayList;
+
+import java.util.ArrayList;
+import java.util.Random;
+
+public class RandomArrayList extends ArrayList {
+
+    public Object getRandomElement(){
+        int index = new Random().nextInt(super.size());
+        Object element = super.get(index);
+        return super.remove(index);
+    }
+}
