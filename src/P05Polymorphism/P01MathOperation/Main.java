642c71338dc52540876a1afc5aa43adfee3260c3
emilyvaseva <114073206+emilyvaseva@users.noreply.github.com>
2023-03-19 16:43:56 +0200

    add task

---
+++ src/P05Polymorphism/P01MathOperation/Main.java
@@ -0,0 +1,9 @@
+package P05Polymorphism.P01MathOperation;
+
+public class Main {
+    public static void main(String[] args) {
+        MathOperation.add(1,2);
+        MathOperation.add(1,2,3);
+        MathOperation.add(1,2,3,4);
+    }
+}
