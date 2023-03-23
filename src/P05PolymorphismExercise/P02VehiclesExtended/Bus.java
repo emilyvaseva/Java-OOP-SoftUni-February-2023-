bb6fc01bdccdcedabb72d441a61637265e4cb29b
emilyvaseva <114073206+emilyvaseva@users.noreply.github.com>
2023-03-23 12:52:57 +0200

    add task

---
+++ src/P05PolymorphismExercise/P02VehiclesExtended/Bus.java
@@ -0,0 +1,10 @@
+package P05PolymorphismExercise.P02VehiclesExtended;
+
+public class Bus extends VehicleImpl {
+
+    public final static Double AC_ADDITIONAL_CONSUMPTION = 1.4;
+
+    public Bus(Double fuelQuantity, Double fuelConsumption, Double tankCapacity) {
+        super(fuelQuantity, fuelConsumption, tankCapacity, AC_ADDITIONAL_CONSUMPTION);
+    }
+}
---
+++ src/P05PolymorphismExercise/P02VehiclesExtended/Vehicle.java
@@ -0,0 +1,8 @@
+package P05PolymorphismExercise.P02VehiclesExtended;
+
+public interface Vehicle {
+
+    String drive(Double distance);
+    void refuel(Double litters);
+    String driveAC(Double distance);
+}
