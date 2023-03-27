bc5acd61491080b9fd5d3f48fd9575ad04c69897
emilyvaseva <114073206+emilyvaseva@users.noreply.github.com>
2023-03-27 10:36:00 +0300

    add task

---
+++ src/P06SOLID/p03_LiskovSubstitution/p01_Square/Shape.java
@@ -0,0 +1,5 @@
+package P06SOLID.p03_LiskovSubstitution.p01_Square;
+
+public interface Shape {
+     double getArea();
+}
---
+++ src/P06SOLID/p04_InterfaceSegregation/p02_identity/AccountController.java
@@ -0,0 +1,14 @@
+package P06SOLID.p04_InterfaceSegregation.p02_identity;
+
+import P06SOLID.p04_InterfaceSegregation.p02_identity.interfaces.Account;
+
+public class AccountController {
+    private final Account manager;
+
+    public AccountController(Account manager) {
+        this.manager = manager;
+    }
+    public void changePassword(String oldPass,String newPass){
+        this.manager.changePassword(oldPass,newPass);
+    }
+}
