0d8f2030936bc351674b3e8cb46099a584922c95
emilyvaseva <114073206+emilyvaseva@users.noreply.github.com>
2023-04-03 18:22:29 +0300

    add task

---
+++ src/P08ExceptionsAndErrorHandling/P02SquareRoot/Main.java
@@ -0,0 +1,28 @@
+package P08ExceptionsAndErrorHandling.P02SquareRoot;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.nextLine();
+
+        try{
+            double sqrt = sqrt(input);
+            System.out.printf("%.2f%n",sqrt);
+        }catch (IllegalArgumentException ignored){
+            System.out.println("Invalid");
+        }
+
+        System.out.println("Goodbye");
+    }
+
+    private static double sqrt(String input) {
+        int number = Integer.parseInt(input);
+
+        if (number<0){
+            throw new IllegalArgumentException("Number " + number + " is negative");
+        }
+        return Math.sqrt(number);
+    }
+}
